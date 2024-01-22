f8c783e0df417c6056bdf67974f191151b18ef84
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2024-01-22 17:02:12 -0300

    hero style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -9,11 +10,37 @@ const Hero = () => (
     >
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraphkk} ml-2`}>
+        <p className={`${styles.paragraph} ml-2`}>
           <span className="text-white">20%</span> Discount For{" "}
           <span className="text-white">1 Month</span> Account
         </p>
       </div>
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation</span>{" "}
+        </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
+      </div>
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Criative Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia
+        dapibus ligula nec lobortis. Etiam id nulla volutpat, volutpat eros
+        vulputate, ornare elit. Fusce vitae eros eget ipsum faucibus laoreet.
+        Integer non quam vel nisl varius malesuada mollis nec augue.
+      </p>
+    </div>
+    <div>
+      <img
+        src={robot}
+        alt="billing"
+        className="w=[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
     </div>
   </section>
 );
