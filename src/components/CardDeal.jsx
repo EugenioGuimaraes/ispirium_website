85f4eb5b4a5eba51fd7f510857c2537ad9df0777
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2024-01-23 15:56:19 -0300

    Feat: add cardDeal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+      Lorem ipsum <br className="sm:block hidden" /> dolor sit amet
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia
+        dapibus ligula nec lobortis. Etiam id nulla volutpat, volutpat.
+      </p>
 
-export default CardDeal
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
